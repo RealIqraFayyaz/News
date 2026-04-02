eeea7432578b10388e23d4b0acc0dd9e183e739e
Iqra Fayyaz <iqrafayyaz0@gmail.com>
2026-04-03 01:53:30 +0500

    Add files via upload

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import Nav from "./nav";
+
+export default function Header({ getKeywords }) {
+
+    const [active, setActive] = useState(false);
+    const [keywords, setKeywords] = useState('');
+
+    return (
+        <>
+            <header style={{ background: `${active ? "purple" : "pink"}` }}>
+                <div className="logo">Awesome News</div>
+
+                <input onChange={getKeywords} />
+
+                <Nav />
+            </header>
+        </>
+    );
+}
---
+++ src/utils/data.js
@@ -0,0 +1,31 @@
+export const navItems = [
+        {Id: '1', link: '/articles', title: 'Article'},
+        {Id: '2', link: '/sports', title: 'Sports'},
+        {Id: '3', link: '/music', title: 'Music'},
+        {Id: '4', link: '/art', title: 'Art'},
+        {Id: '5', link: '/history', title: 'History'},
+    ]
+
+export const newsData = [
+      {
+        "id":1,
+        "title":"New ES6 is available",
+        "feed": "the news is fake i can tell you  because i know everything that is happening there so you should give me respect because I'm the great"
+      },
+       {
+        "id":2,
+        "title":"The importance of react in development",
+        "feed": "we can do hard work but we have to be consistent okay so dont be afraid you will do it anyways "
+      },
+       {
+        "id":3,
+        "title":"React developers going on stike",
+        "feed": "we can do hard work but we have to be consistent okay so dont be afraid you will do it anyways yes "
+      },
+       {
+        "id":4,
+        "title":"Say what again .I dare you",
+        "feed": "we can do hard work but we have to be consistent okay so dont be afraid you will do it anyways okayy"
+      },
+    ]
+    
